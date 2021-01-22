4d3f385c497710b6ef143152962802c6c45d974f
Abel Mescua <abelmescua@gmail.com>
2021-01-22 20:27:18 +0100

    Text Generation with LSTMs with Keras and Python - Part Two
    Creating our model with our data (X,y)
    
    Saving model and tokenizer

--- 06-Deep-Learning/01-Text-Generation-with-Neural-Networks2.py
+++ 06-Deep-Learning/01-Text-Generation-with-Neural-Networks2.py
@@ -131,6 +131,98 @@ sequences
 
 
 # %%
+# part 2: Create our data (X, y)
+from keras.utils import to_categorical
+
+
+# %%
+sequences[:, :-1] # each row of words (features)
+
+
+# %%
+sequences[:,-1] # last word of each row (label)
+
+
+# %%
+X = sequences[:, :-1] # features
+
+
+# %%
+y = sequences[:,-1] # label
+
+
+# %%
+y = to_categorical(y, num_classes=vocabulary_size+1)
+
+
+# %%
+X.shape # (sequences, words per sequence)
+
+
+# %%
+seq_len = X.shape[1]
+
+
+# %%
+# Create our model with the data
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Embedding # LSTM: For the sequences, Embedding: for the words
+
+
+# %%
+# function to create models
+def create_model(vocabulary_size, seq_len):
+
+    model = Sequential()
+
+    model.add(Embedding(vocabulary_size, seq_len, input_length=seq_len))
+    # LSTM layer chooses amount of neurons, recommended to have as amount a multiple of seq_len
+    model.add(LSTM(seq_len*2, return_sequences=True)) # first param should be multiple of seq_len (Recomended)
+    model.add(LSTM(seq_len*2))
+
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(vocabulary_size, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.summary()
+
+    return model
+
+
+# %%
+model = create_model(vocabulary_size+1, seq_len) # creating our model
+
+
+# %%
+# training our model
+
+
+# %%
+# to save files for later and load them
+from pickle import dump, load
+
+
+# %%
+model.fit(
+    X, y,
+    batch_size=128, # how many sequences do you want to pass at a time
+    epochs=2, # number of tranings in the execution
+    verbose=1 # output report
+)
+
+
+# %%
+# saving our model
+model.save('my_mobydick_model.h5')
+
+
+# %%
+# tokenizer from Keras with all the vocabsulary of the documents
+dump(tokenizer,open('my_simpletokenizer','wb')) # saving our token
+
+
+# %%
 
 
 
